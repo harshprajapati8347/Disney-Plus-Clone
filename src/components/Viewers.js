87047d0af1efd16ffe7dba98db968020d3228a68
Harsh Prajapati <64133891+harshprajapati8347@users.noreply.github.com>
2021-05-06 21:52:41 +0530

    Viewers Done

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -8,6 +8,18 @@ const Viewers = () => {
         <Wrap>
           <img src="/images/viewers-disney.png" alt="viewers" />
         </Wrap>
+        <Wrap>
+          <img src="/images/viewers-pixar.png" alt="viewers" />
+        </Wrap>
+        <Wrap>
+          <img src="/images/viewers-marvel.png" alt="viewers" />
+        </Wrap>
+        <Wrap>
+          <img src="/images/viewers-starwars.png" alt="viewers" />
+        </Wrap>
+        <Wrap>
+          <img src="/images/viewers-national.png" alt="viewers" />
+        </Wrap>
       </Container>
     </>
   );
@@ -16,22 +28,32 @@ const Viewers = () => {
 export default Viewers;
 
 const Container = styled.div`
+  grid-template-columns: repeat(5, minmax(0, 1fr));
   margin-top: 30px;
   display: grid;
   grid-gap: 25px;
   padding: 30px 0px 26px;
-  grid-template-columns: repeat(5, minmax(0, 1fr));
 `;
 
 const Wrap = styled.div`
   border-radius: 10px;
   border: 3px solid rgba(249, 249, 249, 0.1);
-  box-shadow: rgb(0 0 0/ 69%) 0px 26px 30px -10px,
-    rbg(0 0 0 / 73%) 0px 16px 10px -10px;
+  box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px;
+  // rbg(0 0 0 / 73%) 0px 16px 10px -10px;
+  cursor: pointer;
+  transition:all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94);
 
   img {
     width: 100%;
     height: 100%;
     object-fit: cover;
+  }
+
+  &:hover {
+    transform: scale(1.05);
+    border-color: rgba(249, 249, 249, 0.8);
+    box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px;
+    rbg(0 0 0 / 72%) 0px 30px 22px -10px;
+    
   }
 `;

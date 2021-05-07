e78a62aa3b6567805490c284dcc6832bd0804d5e
Harsh Prajapati <64133891+harshprajapati8347@users.noreply.github.com>
2021-05-07 14:57:59 +0530

    Detail Page Done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Detail from "./components/Detail";
 
 function App() {
   return (
     <>
       <div className="App">
-        <Header />
-        <Home />
+        <Router>
+          <Header />
+          <Switch>
+            <Route exact path="/detail">
+              <Detail />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
       </div>
     </>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
-import styled from "styled-components";
+import styled from "styled-components";   
 import ImgSlider from "./ImgSlider";
+import Movies from "./Movies";
 import Viewers from "./Viewers";
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
       <Container>
         <ImgSlider />
         <Viewers />
+        <Movies />
       </Container>
     </>
   );
